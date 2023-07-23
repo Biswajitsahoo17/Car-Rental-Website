6779b84a267b24862559c51314e753a8c544df64
Biswajit-17 <119510371+Biswajit-17@users.noreply.github.com>
2023-07-23 11:20:05 +0530

    Initial commit

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+              If a driver decides to rent a car beforehand, 
+              they go to a car rental website or app and find a vehicle that meets their needs. 
+              Then, they book it and pay for the chosen rental period, if upfront payment is required. 
+              On the appointed day, this customer arrives at the car rental location to pick up the chosen car.
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
